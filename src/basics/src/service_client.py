c143859b70cd940ad7f3c2a8099a21b3d44ab1c6
farawell <farawell777@kaist.ac.kr>
2023-12-28 23:02:29 +0900

    Completed service_client.py

--- src/basics/src/service_client.py
+++ src/basics/src/service_client.py
@@ -5,6 +5,14 @@ from basics.srv import WordCount
 import sys
 
 rospy.init_node('service_client')
-rospy.wait_for_service('word_count')
+rospy.wait_for_service('word_count') # Wait for the server to promote the service
+# Utilizing the service before it being promoted will cause error,
+# which is different form topic.
 
+words = ' '.join(sys.argv[1:])
+
+# Allow to use 'word_count' as a local function that calls the service
 word_counter = rospy.ServiceProxy('word_count', WordCount)
+word_count = word_counter(words)
+
+print(words, '->', word_count.count) # final result
