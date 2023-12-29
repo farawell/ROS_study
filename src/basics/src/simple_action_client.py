254f0b7f7c60b965f425ec25d9998fa112030243
farawell <farawell777@kaist.ac.kr>
2023-12-30 02:16:51 +0900

    Adding some untracted files in catkin_ws/devel/

--- src/basics/src/simple_action_client.py
+++ src/basics/src/simple_action_client.py
@@ -7,12 +7,12 @@ from basics.msg import TimerAction, TimerGoal, TimerResult
 rospy.init_node('timer_action_client')
 client = actionlib.SimpleActionClient('timer', TimerAction)
 
-client.wait_for_server()
+client.wait_for_server() # Redady until the server is ready
 
 goal = TimerGoal()
 goal.time_to_wait = rospy.Duration.from_sec(5.0)
-client.send_goal(goal)
+client.send_goal(goal) # Send the goal to the server
 
-client.wait_for_result()
+client.wait_for_result() # Wait for the server repsponse
 
 print('Time elapsed: %f'%(client.get_result().time_elapsed.to_sec()))
